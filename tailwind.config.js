da46bdaf000882d8f312f6d3f8895ee7e36d444b
tian-net <freddy.quispe@vallegrande.edu.pe>
2025-04-08 08:32:28 -0500

    creacion carrusel

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+
+module.exports = {
+    content: [
+        "./app.py/",
+        "./templates/**/*.html",
+        "./static/**/*.js",
+    ],
+    theme: {
+        extend: {
+            colors: {
+                primary: "#1E40AF",
+                secondary: "#F59E0B",
+            },
+        },
+    },
+    Plugins: [],
+};
